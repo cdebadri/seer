32fd37bca7971b0bea48266f12366ad16ec48f5e
Dwikmika <dwikmika.kumar@gmail.com>
2020-08-15 13:22:06 +0530

    edited people page (new components added)
    Signed-off-by: Dwikmika <dwikmika.kumar@gmail.com>

---
+++ src/components/PeopleSearch.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="input-group md-form form-sm form-1 p-2 pt-4">
+      <input
+        class="form-control my-0 py-1 amber-border"
+        type="text"
+        placeholder="Search"
+        aria-label="Search"
+      />
+      <div class="input-group-append">
+        <span class="input-group-text amber lighten-3" id="basic-text1">
+          <i class="fas fa-search text-grey" aria-hidden="true"></i>
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.input-group.md-form.form-sm.form-1 input {
+  border: 1px solid #bdbdbd;
+  border-top-left-radius: 0.25rem;
+  border-bottom-left-radius: 0.25rem;
+}
+
+.input-group.md-form.form-sm.form-1 input.amber-border {
+  border: 1px solid #ffca28;
+}
+.input-group-text {
+  background-color: #ffca28;
+  border: #ffca28;
+}
+</style>
--- src/views/People.vue
+++ src/views/People.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="about">
-    <h1>This is the people page</h1>
+  <div class="row" style="height:550px">
+    <div class="col-3">
+      <PeopleSearch style="background-color: white; height:100%" />
+    </div>
+    <div class="col">
+      <PeopleProfile />
+    </div>
   </div>
 </template>
+<script>
+import PeopleSearch from '@/components/PeopleSearch.vue';
+import PeopleProfile from '@/components/PeopleProfile.vue';
+
+export default {
+  name: 'People',
+  components: {
+    PeopleSearch,
+    PeopleProfile,
+  },
+};
+</script>
